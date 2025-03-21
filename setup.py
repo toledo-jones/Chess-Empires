20eff12b10138e2af96db2173d1ff655ca8f99f0
toledo-jones <bogausch.cody@gmail.com>
2025-03-21 18:28:29 -0400

    Updated setup.py for windows build

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 import cx_Freeze
 
-executables = [cx_Freeze.Executable('main.py', base="Win32GUI")]
+executables = [cx_Freeze.Executable('main.py')]  # No base needed for macOS
 
 cx_Freeze.setup(
     name="Chess Empires",
     options={"build_exe": {
         "packages": ["pygame"],
-        "include_files": [("files", "files")],  # Ensure correct copying
+        "include_files": [("files", "files")],  # Ensure correct file copying
         "excludes": ["cv2"]
     }},
     executables=executables
